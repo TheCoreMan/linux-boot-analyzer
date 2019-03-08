8903cba4d923d245d72f0cd6c11529391ba898b1
TheCoreMan <dude500@gmail.com>
2019-03-09 01:30:28 +0200

    Added argparse support

--- linux_boot_analyzer/collector/collector.py
+++ linux_boot_analyzer/collector/collector.py
@@ -1,3 +1,4 @@
+import argparse
 import glob
 import os
 import pprint
@@ -5,7 +6,24 @@ import pprint
 from parsers import UnitParser
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Analyze linux boot unit files.')
+    parser.add_argument('--output', '-o',
+                        choices=['print', 'file', 'server'],
+                        help='Where to output to')
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+    parsed_units = parse_all_units()
+    if args.output == "print":
+        pprint.pprint(parsed_units)
+    else:
+        raise NotImplementedError(args.output)
+
+
+def parse_all_units():
     path_to_systemd_unit_files = os.path.join("/", "etc", "systemd", "system", "*service")
     systemd_service_files = glob.glob(path_to_systemd_unit_files)
     parsed_units = []
@@ -18,7 +36,7 @@ def main():
                 "inner_info": parsed_unit_info
             }
         )
-    pprint.pprint(parsed_units)
+    return parsed_units
 
 
 if __name__ == '__main__':
