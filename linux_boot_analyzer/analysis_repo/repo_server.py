ae5e7c424889b0172cfaa7fc16602dad92edcfaf
TheCoreMan <dude500@gmail.com>
2019-03-09 21:36:34 +0200

    Added basic server code, connects to Google SQL Cloud I've set up.

---
+++ linux_boot_analyzer/analysis_repo/repo_server.py
@@ -0,0 +1,76 @@
+"""
+Code loosely based on http://www.postgresqltutorial.com/postgresql-python/connect/
+"""
+
+from configparser import ConfigParser
+
+import psycopg2
+
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config(filename='database.ini', section='postgresql')
+        """
+        THIS FILE WILL NOT BE ON SOURCE CONTROL. 
+        Create a "secret.ini" file like this: 
+        
+            [postgresql]
+            password=PUT_DATABASE_PASSWORD_HERE
+        
+        """
+        secret = config(filename='secret.ini', section='postgresql')
+        params["password"] = secret["password"]
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        print("Config parameters:" + str(params))
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def main():
+    connect()
+
+
+if __name__ == '__main__':
+    main()
