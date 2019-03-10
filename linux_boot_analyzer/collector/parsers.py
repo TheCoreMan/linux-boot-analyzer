3743c6ad36f553c9e6e261936ccc2201eb81dc74
TheCoreMan <dude500@gmail.com>
2019-03-10 02:05:03 +0200

    Moved list of systems to common.py and improved the unit parser to include sysv as well.

--- linux_boot_analyzer/collector/parsers.py
+++ linux_boot_analyzer/collector/parsers.py
@@ -2,6 +2,8 @@ import configparser
 import hashlib
 from collections import OrderedDict
 
+import common
+
 
 def file_as_bytes(file):
     with file:
@@ -12,29 +14,41 @@ class UnitParser:
     system = ""
 
     def __init__(self, system):
-        self.system = system
+        if system in common.SYSTEM_TO_UNIT_FILE_GLOB:
+            self.system = system
+        else:
+            raise NotImplementedError()
 
     def parse_unit(self, path):
         # Todo currently this is some sort of a factory method for ease of extendability. Need to improve this class's
         #  design - for testability of different parsers, probably inject method. Read on factory DP in python
-        if self.system == "systemd":
-            parsed = parse_systemd(path)
-
-            results = {
-                "system": "systemd",
-                "Description": parsed["Unit"]["Description"],
-                "ExecStart": parsed["Service"]["ExecStart"],
-                # todo this is probably memory inefficent, but meh.
+        results = {
+            "system": self.system,
+            # todo this is probably memory inefficent, but meh.
+            "hashes": {
                 "md5": hashlib.md5(file_as_bytes(open(path, 'rb'))).hexdigest(),
                 "sha-256": hashlib.sha256(file_as_bytes(open(path, 'rb'))).hexdigest()
             }
+        }
+        if self.system == "systemd":
+            parsed = parse_systemd(path)
 
+            results["Description"] = parsed["Unit"]["Description"]
+            results["ExecStart"] = parsed["Service"]["ExecStart"]
+            results["ImagePath"] = parsed["Service"]["ExecStart"].split()[0]
+            try:
+                results["ExecHash"] = hashlib.sha256(file_as_bytes(open(results["ImagePath"], 'rb'))).hexdigest()
+            except IOError as err:
+                pass  # Might be permissions or nonexistant file.
+                # TODO add logging mechanism to collector.
             if "User" in parsed["Service"]:
                 results["User"] = parsed["Service"]["User"]
             if "Group" in parsed["Service"]:
                 results["User"] = parsed["Service"]["Group"]
 
-            return results
+        # todo elif self.system == "sysv": shlex and parse the executable from the "start" directive if exists.
+
+        return results
 
 
 class MultiOrderedDict(OrderedDict):
